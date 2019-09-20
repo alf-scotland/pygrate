eb1561b4554289c60b62a1262a5bb3d685d5ef3c
Alf Scotland <alf.scotland@roche.com>
2019-09-20 17:30:02 +0200

    Turning into a package and adding migration

---
+++ pygrate/migrate.py
@@ -0,0 +1,143 @@
+import os
+import argparse
+import logging
+import shutil
+from collections import defaultdict
+from pathlib import Path
+
+from openpyxl import load_workbook
+from openpyxl.worksheet.worksheet import Worksheet, Cell
+
+
+LOG = logging.getLogger(__name__)
+
+
+def read_migration_sheet(path, sheet_name=None):
+    """ Simple reader to ingest migration sheets """
+    wb = load_workbook(path)
+    sheet = wb[sheet_name] if sheet_name else wb.active
+    return sheet
+
+
+class Action:
+    def __init__(
+        self,
+        action,
+        priority,
+        source,
+        target
+    ):
+        if not action or action.lower() not in ('move', 'delete'):
+            raise ValueError(f'Unknown action: {action}')
+
+        self.action = action.lower()
+        self.priority = priority
+        self.source = source
+
+        if self.action == 'move' and not target:
+            raise ValueError('Target needs to be specified for action move')
+
+        self.target = target
+    
+    def __repr__(self):
+        res = f'{self.action} {self.source}'
+        if self.action.lower() == 'move':
+            res += f' -> {self.target}'
+        return res
+    
+    def validate(self):
+        # check if source exists
+        if not self.source.exists():
+            raise Exception(f'Source does not exist: {self.source}')
+    
+    def _delete(self):
+        if self.source.is_file():
+            self.source.unlink()
+        else:
+            shutil.rmtree(str(self.source))
+    
+    def _move(self):
+        shutil.move(str(self.source), str(self.target))
+    
+    def perform(self):
+        if self.action == 'delete':
+            self._delete()
+        elif self.action == 'move':
+            self._move()
+        else:
+            raise ValueError(f'Unknown action: {self.action}')
+
+    __str__ = __repr__
+
+
+def sheet_to_actions(sheet: Worksheet):
+    iter_ = sheet.iter_rows()
+    next(iter_)  # skip header
+
+    # collect actions and paths
+    actions = {}
+    paths = []
+    for row in iter_:
+        path, _, _, _, action, target, *_ = map(lambda x: x.value, row)
+
+        path = Path(path)
+        target = Path(target) if target else None
+
+        if action:
+            action_cls = Action(action, len(path.parents), path, target)
+            LOG.info(f'Found action: {action_cls}')
+            actions[path] = action_cls
+        else:
+            paths.append(path)
+    
+    # check if all paths are addressed
+    for path in paths:
+        has_parent_action = any(p in actions for p in path.parents)
+        
+        if not has_parent_action:
+            LOG.warning(f'{path} has no action')
+    
+    return actions
+
+
+def _prioritize_actions(actions):
+    return sorted(actions.values(), key=lambda a: a.priority, reverse=True)
+
+
+def perform_actions(actions):
+    """ Perform actions """
+    for action in _prioritize_actions(actions):
+        LOG.info(f'About to perform: {action}')
+        action.perform()
+        LOG.info(f'Action performed: {action}')
+
+
+def dry_run_actions(actions):
+    for action in _prioritize_actions(actions):
+        LOG.info(f'Would perform action: {action}')
+
+
+def migrate(workbook_path, sheet_name, dry_run=False):
+    sheet = read_migration_sheet(workbook_path, sheet_name)
+    actions = sheet_to_actions(sheet)
+    if dry_run:
+        dry_run_actions(actions)
+    else:
+        perform_actions(actions)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('workbook')
+    parser.add_argument('--sheet')
+    parser.add_argument('--dry-run', action='store_true')
+    args = parser.parse_args()
+
+    # configure logging
+    logging.basicConfig(level=logging.INFO)
+
+    migrate(args.workbook, args.sheet, args.dry_run)
+
+
+if __name__ == '__main__':
+    main()
